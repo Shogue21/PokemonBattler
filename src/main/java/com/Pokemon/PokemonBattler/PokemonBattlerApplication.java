21bb7b72b62a49ea776ddce3bb44cf70bf572c94
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-06-07 10:12:09 -0500

    Initial commit

---
+++ src/main/java/com/Pokemon/PokemonBattler/PokemonBattlerApplication.java
@@ -0,0 +1,20 @@
+package com.Pokemon.PokemonBattler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PokemonBattlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonBattlerApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+}
---
+++ src/test/java/com/Pokemon/PokemonBattler/PokemonBattlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pokemon.PokemonBattler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonBattlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
